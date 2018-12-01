fde69bc7c0eb8fca5d993dc16f14e064f7ab61e3
Mrsirha <mrsirha@gmail.com>
2018-12-01 12:26:06 +1100

    Please, I have no idea what im doing

---
+++ main.py
@@ -0,0 +1,6 @@
+#The Sil Bot
+import time
+from random import randint as rand
+
+file_object = open("phrases.txt","r").read()
+lines = file_object.splitlines()
---
+++ sil.py
@@ -0,0 +1,10 @@
+#the sil object that decides the phrase and time that the phrase is uttered
+
+class sil:
+    def sil(self, phrases, time):
+        self.phrases = phrases
+        self.time = time
+
+    def pickPhrase():
+        
+    def pickTime():
