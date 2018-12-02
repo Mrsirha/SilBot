88b21e3d3a978d2068415fb1cbaa1cbc161e5082
Mrsirha <mrsirha@gmail.com>
2018-12-02 12:40:08 +1100

    tweaks for usability

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import random
 import keyboard as Keyboard
 from pynput.keyboard import Key, Controller
 
-keyboard = Controller()
+keypress = Controller()
 file_object = open("Sil.txt","r").read()
 lines = file_object.splitlines()
 
 def Menu(): # A menu option (WIP - need to add functionality for Discord, input, and expected response)
-    print("Welcome to SilBot \n 1.Begin typing as Sil \n 2.Exit")
+    print("Welcome to SilBot \n1.Begin typing as Sil \n2.Exit")
     choice = input()
     if choice == "1":
         beginTyping()
@@ -20,24 +20,25 @@ def Menu(): # A menu option (WIP - need to add functionality for Discord, input,
         exit()
 
 def beginTyping():
-    print("Prepare to tpye as sil")
-    time.sleep(3)
+    print("Prepare to type as sil")
     i = 0
     while True:
         try:
-            sil = Sil(lines, time.time(), i)
-            time.sleep(sil.getDeath())
-            print("TPYING: " + sil.getPhrase())
-            for char in sil.getPhrase():
-                keyboard.press(char)
-                keyboard.release(char)
-                time.sleep(0.001)
-            time.sleep(0.1)
-            keyboard.press(Key.enter)
-            if Keyboard.is_pressed('2'):
+            if Keyboard.is_pressed(2):
                 print("Exiting...")
                 break
-            i += 1
+            sil = Sil(lines, time.time(), i)
+            if time.time() == sil.getAge() + sil.getDeath():
+                print("TPYING: " + sil.getPhrase())
+                for char in sil.getPhrase():
+                    keypress.press(char)
+                    keypress.release(char)
+                    time.sleep(0.001)
+                time.sleep(0.1) # sometime the enter keypress does not register if it is too soon after the rest of the keypresses
+                keypress.press(Key.enter)
+                keypress.release(Key.enter)
+                i += 1
+
         except:
             print("There was an error...")
 
